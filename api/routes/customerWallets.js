5da2ac46c40058053f775455aa225ea7cfebdbbd
Bruno Brito <xbrunots@gmail.com>
2021-01-24 17:39:02 -0300

    l

--- api/routes/customerWallets.js
+++ api/routes/customerWallets.js
@@ -1,11 +1,11 @@
 module.exports = app => {
   const controller = app.controllers.customerWallets;
 
-  app.route('/api/v1/customer-wallets')
+  app.route('/v1/login')
     .get(controller.listCustomerWallets)
     .post(controller.saveCustomerWallets);
 
-  app.route('/api/v1/customer-wallets/:customerId')
+  app.route('/v1/login/:customerId')
     .delete(controller.removeCustomerWallets)
     .put(controller.updateCustomerWallets);
 }
